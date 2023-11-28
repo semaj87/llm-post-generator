8ca3d25ea2f68d94200cf063716172f9add1ca82
James Aymer <aymerjames@gmail.com>
2023-11-28 10:51:16 +0000

    WIP

---
+++ README.md
@@ -0,0 +1,25 @@
+# LLM post generator
+Using an LLM to generate a post for a given topic
+
+
+## System design
+
+
+## Prerequisites
+
+
+## Models
+
+
+## Run App Locally
+
+```bash
+source build.sh
+```
+
+## Run App with Streamlit Cloud
+
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
---
+++ libs/helper.py
@@ -0,0 +1,7 @@
+messages = {
+    "search_for_articles": ["Searching for the top articles...", "Articles found."],
+    "find_the_best_article_urls": ["Searching for the top URL...", "URL found."],
+    "get_content_from_urls": ["Extracting content from the URL...", "Content extracted."],
+    "summarise_content": ["Summarising the content...", "Content summarised."],
+    "generate_reddit_post": ["Generating the post...", "Post generated"]
+}
